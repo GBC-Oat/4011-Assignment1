d3c9621dca9861ae15978a173c58503714a02cb0
GBC-Oat <126524208+GBC-Oat@users.noreply.github.com>
2024-01-18 23:44:53 -0500

    function implemented in rmse.py

--- Assignment2/rmse.py
+++ Assignment2/rmse.py
@@ -3,5 +3,5 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = np.sqrt(np.mean((pred-tar)**2)) # TODO: Implement RMSE Calculation here...
+    rmse = np.sqrt(np.mean((pred-tar)**2)) # function implemented
     return rmse
