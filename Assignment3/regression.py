8eca19157da076c1254018dc82864b905f367f99
GBC-Oat <126524208+GBC-Oat@users.noreply.github.com>
2024-01-23 18:32:45 -0500

    Update regression.py

--- Assignment3/regression.py
+++ Assignment3/regression.py
@@ -31,24 +31,21 @@ def fit_regression_model(X, y):
     Hint 3: you can use the previos_loss variable to stop the training when the loss is not changing much.
     """
     learning_rate = 0.01 # Pick a better learning rate
-    num_epochs = 1000 # Pick a better number of epochs
-    input_features = X.shape[1] # extract the number of features from the input `shape` of X
-    output_features = y.shape[1] # extract the number of features from the output `shape` of y
+    num_epochs = 100 # Pick a better number of epochs
+    input_features = 0 # extract the number of features from the input `shape` of X
+    output_features = 0 # extract the number of features from the output `shape` of y
     model = create_linear_regression_model(input_features, output_features)
     
     loss_fn = nn.L1Loss() # Use mean squared error loss, like in class
 
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    previous_loss = float("inf")
+    previos_loss = float("inf")
 
     for epoch in range(1, num_epochs):
         loss = train_iteration(X, y, model, loss_fn, optimizer)
-        if abs(previous_loss - loss.item()) < 0.001: # This condition changed to stop the training when the loss is not changing much.
+        if False: # Change this condition to stop the training when the loss is not changing much.
             break
-        previous_loss = loss.item()
-        if epoch % 100 == 0:
-            print(f'Epoch {epoch}/{num_epochs}, Loss: {loss.item()})')
+        previos_loss = loss.item()
         # This is a good place to print the loss every 1000 epochs.
     return model, loss
-
