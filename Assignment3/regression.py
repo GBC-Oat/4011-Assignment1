75668d4306d9aa142ee3af2699b65aea8d4511d0
GBC-Oat <126524208+GBC-Oat@users.noreply.github.com>
2024-01-23 21:35:31 -0500

    Add files via upload

--- Assignment3/regression.py
+++ Assignment3/regression.py
@@ -30,10 +30,10 @@ def fit_regression_model(X, y):
     Hint 2: while woring you can use the print function to print the loss every 1000 epochs.
     Hint 3: you can use the previos_loss variable to stop the training when the loss is not changing much.
     """
-    learning_rate = 0.01 # Pick a better learning rate
-    num_epochs = 100 # Pick a better number of epochs
-    input_features = 0 # extract the number of features from the input `shape` of X
-    output_features = 0 # extract the number of features from the output `shape` of y
+    learning_rate = 0.01 # Adjust the learning rate
+    num_epochs = 1000 # Adjust the number of epochs
+    input_features = X.shape[1] # extract the number of features from the input `shape` of X
+    output_features = y.shape[1] # extract the number of features from the output `shape` of y
     model = create_linear_regression_model(input_features, output_features)
     
     loss_fn = nn.L1Loss() # Use mean squared error loss, like in class
@@ -44,8 +44,10 @@ def fit_regression_model(X, y):
 
     for epoch in range(1, num_epochs):
         loss = train_iteration(X, y, model, loss_fn, optimizer)
-        if False: # Change this condition to stop the training when the loss is not changing much.
+        if abs(previos_loss - loss.item()) < 0.001:
             break
         previos_loss = loss.item()
-        # This is a good place to print the loss every 1000 epochs.
+        if epoch % 100 == 0:
+            print(f'Epoch {epoch}/{num_epochs}, Loss: {loss.item()}') # This is a good place to print the loss every 1000 epochs.
     return model, loss
+
